f8cc0cfccccb8d8f827e94235a69321bd54a4023
zzm <zzmwork8@gmail.com>
2022-04-19 11:06:37 +0800

    genesis election

--- pallets/staking-extend/src/elect.rs
+++ pallets/staking-extend/src/elect.rs
@@ -1,13 +1,17 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use frame_election_provider_support::{ElectionDataProvider, ElectionProvider};
-use sp_npos_elections::Supports;
 use frame_support::pallet_prelude::PhantomData;
+use sp_npos_elections::Supports;
 
 pub struct OnChainSequentialPhragmen<T: Config>(PhantomData<T>);
 
 pub trait Config: frame_system::Config {
 	type ElectionProvider: ElectionProvider;
+
+	/// Something that provides the election functionality at genesis.
+	type GenesisElectionProvider: ElectionProvider;
+
 	type DataProvider: ElectionDataProvider<
 		AccountId = <Self::ElectionProvider as ElectionProvider>::AccountId,
 		BlockNumber = <Self::ElectionProvider as ElectionProvider>::BlockNumber,
@@ -16,16 +20,20 @@ pub trait Config: frame_system::Config {
 
 impl<T: Config> ElectionProvider for OnChainSequentialPhragmen<T> {
 	type AccountId = <T::ElectionProvider as ElectionProvider>::AccountId;
-	type BlockNumber = <T::ElectionProvider as ElectionProvider>::BlockNumber ;
+	type BlockNumber = <T::ElectionProvider as ElectionProvider>::BlockNumber;
 	// type Error = <T::ElectionProvider as ElectionProvider>::Error ;
 	// type DataProvider = <T::ElectionProvider as ElectionProvider>::DataProvider;
 
 	// type AccountId = T::AccountId;
 	// type BlockNumber = T::BlockNumber ;
-	type Error = <T::ElectionProvider as ElectionProvider>::Error ;
-	type DataProvider = T::DataProvider ;
+	type Error = <T::ElectionProvider as ElectionProvider>::Error;
+	type DataProvider = T::DataProvider;
 
 	fn elect() -> Result<Supports<Self::AccountId>, Self::Error> {
+		let current = <frame_system::Pallet<T>>::block_number();
+		if current == T::BlockNumber::default() {
+			T::GenesisElectionProvider::elect()
+		}
 		T::ElectionProvider::elect()
 	}
 }
